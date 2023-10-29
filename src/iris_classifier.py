b85b346d5046d51210e28f66caded1972d71ee9c
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-10-30 00:44:47 +0300

    load dataset

--- src/iris_classifier.py
+++ src/iris_classifier.py
@@ -2,3 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+X = iris.data  # Features
+y = iris.target  # Target (class labels)
