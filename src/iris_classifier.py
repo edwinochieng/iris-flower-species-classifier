00775d179e945936312f18e15e349fc972b2a505
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-10-30 00:54:41 +0300

    split dataset into training and testing sets

--- src/iris_classifier.py
+++ src/iris_classifier.py
@@ -6,3 +6,6 @@ from sklearn.metrics import accuracy_score
 iris = load_iris()
 X = iris.data  # Features
 y = iris.target  # Target (class labels)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
